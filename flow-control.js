0bcf53ff87fdbae720ecdc0444063ffc61a2858f
Aditi Ohri <didiohri@gmail.com>
2019-07-18 19:47:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,24 +1,27 @@
 function basicTeenager(age) {
-if (13, 14, 15, 16, 17, 18, 19)
-return 'You are a teenager!'
+if (13, 14, 15, 16, 17, 18, 19) {
+return 'You are a teenager!' }
 }
 
 function teenager(age) {
-  if (13, 14, 15, 16, 17, 18, 19)
+  if (13, 14, 15, 16, 17, 18, 19) {
   return 'You are a teenager!' }
 else { return 'You are not a teenager'
 }
+}
 
 function ageChecker(age) {
-  if (13, 14, 15, 16, 17, 18, 19)
+  if (13, 14, 15, 16, 17, 18, 19) {
 return 'You are a teenager!'}
-else if { (12>=12) return 'You are a kid'}
-else { (20<=20) return 'You are a grownup'
+else if  (12>=12) { return 'You are a kid'}
+else  (20<=20) { return 'You are a grownup'
+}
 }
 
-function ternaryTeenager(age) {
-
-}
+function ternaryTeenager(age)
+  if (13, 14, 15, 16, 17, 18, 19)
+return 'You are a teenager' :
+'You are not a teenager'
 
 function switchAge(age) {
 
