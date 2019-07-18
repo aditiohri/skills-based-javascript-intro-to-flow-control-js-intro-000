2f440769b1712530542199616a532590ada4442f
Aditi Ohri <didiohri@gmail.com>
2019-07-18 19:58:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,14 +4,14 @@ return 'You are a teenager!' }
 }
 
 function teenager(age) {
-  if (13, 14, 15, 16, 17, 18, 19) {
+  if (13<=19) {
   return 'You are a teenager!' }
 else { return 'You are not a teenager'
 }
 }
 
 function ageChecker(age) {
-  if (13, 14, 15, 16, 17, 18, 19) {
+  if (13<=19) {
 return 'You are a teenager!'}
 else if  (12>=12) { return 'You are a kid'}
 else  (20<=20) { return 'You are a grownup'
